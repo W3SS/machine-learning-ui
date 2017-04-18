37d68fe60d44f7927a560d29f046b6f98f112f37
vchaillou <valentin.chaillou@laposte.net>
2017-04-18 21:04:44 +0200

    Add dll function calls
    So much functions ! Wow !

--- Assets/MyFirstDLLWrapper.cs
+++ Assets/MyFirstDLLWrapper.cs
@@ -5,9 +5,43 @@ public static class MyFirstDLLWrapper
 {
     private static Type myType;
 
-    public static int return42()
+    public static IntPtr linear_create_model(int inputDimension)
     {
-        return (int) callFunction("return42", new object[] {});
+        return (IntPtr) callFunction("linear_create_model", new object[] {inputDimension});
+    }
+
+    public static void linear_remove_model(IntPtr model)
+    {
+        callFunction("linear_remove_model", new object[] {model});
+    }
+
+    public static int linear_fit_regression(IntPtr model, double[] inputs, int inputsSize, int inputSize, double[] outputs, int outputSize)
+    {
+        return (int) callFunction("linear_fit_regression", new object[] {model, inputs, inputSize, outputs});
+    }
+
+    public static int linear_fit_classification_hebb(IntPtr model, double[] inputs, int inputsSize, int inputSize,
+        int iterationNumber, double step)
+    {
+        return (int)callFunction("linear_fit_classification_hebb",
+                new object[] {model, inputs, inputSize, iterationNumber, step});
+    }
+
+    public static int linear_fit_classification_rosenblatt(IntPtr model, double[] inputs, int inputsSize, int inputSize,
+        double[] outputs, int outputSize, int iterationNumber, double step)
+    {
+        return (int)callFunction("linear_fit_classification_rosenblatt",
+                new object[] {model, inputs, inputSize, outputs, iterationNumber, step});
+    }
+
+    public static double linear_classify(IntPtr model, double[] input, int inputSize)
+    {
+        return (double) callFunction("linear_classify", new object[] {model, input});
+    }
+
+    public static double linear_predict(IntPtr model, double[] input, int inputSize)
+    {
+        return (double) callFunction("linear_predict", new object[] {model, input});
     }
 
     private static object callFunction(string functionName, object[] param)
@@ -19,5 +53,10 @@ public static class MyFirstDLLWrapper
     static MyFirstDLLWrapper()
     {
         myType = Assembly.LoadFile(@"./Assets/2017-5A-AL2-MyFirstNativeDllForUnity.dll").GetType("MyDll.Source");
+    }
+
+    class model
+    {
+
     }
 }
