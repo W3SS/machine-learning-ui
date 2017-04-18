65dc5e0a283ac905d1644c2165fd4e3a37564232
vchaillou <valentin.chaillou@laposte.net>
2017-04-18 12:49:15 +0200

    First commit
    Much wow !

---
+++ Assets/MyFirstDLLWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+public static class MyFirstDLLWrapper
+{
+    private static Type myType;
+
+    public static int return42()
+    {
+        return (int) callFunction("return42", new object[] {});
+    }
+
+    private static object callFunction(string functionName, object[] param)
+    {
+        MethodInfo m = myType.GetMethod(functionName);
+        return m.Invoke(null, param);
+    }
+
+    static MyFirstDLLWrapper()
+    {
+        myType = Assembly.LoadFile(@".\Assets\2017-5A-AL2-MyFirstNativeDllForUnity.dll").GetType("MyDll.Source");
+    }
+}
